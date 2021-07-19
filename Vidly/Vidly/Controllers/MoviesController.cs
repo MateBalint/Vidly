494c61b25cfc34a6b1000a415cd0ccaf1d20cc13
Máté Bálint <matebalint98@gmail.com>
2021-07-20 01:31:10 +0200

    MoviesController has been extended

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -13,15 +13,28 @@ namespace Vidly.Controllers
                 Name = "Shrek"
             };
 
-            //return View(movie);
-
-            //return Content("Hello world!");
-
-            //return HttpNotFound();
-
-            //return new EmptyResult();
-
             return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //movies 
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
